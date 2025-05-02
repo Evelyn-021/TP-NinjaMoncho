cc3245e41449a571ca58c951ee3700a72cef4a7b
Evelyn-021 <evelynalonso11@hotmail.com>
2025-05-02 17:39:42 -0300

    Mejora 4 (Más plataformas)

--- scenes/NinjaMoncho.js
+++ scenes/NinjaMoncho.js
@@ -37,7 +37,12 @@ export default class NinjaMoncho extends Phaser.Scene {
 
     //Plataformas
     this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    this.platforms.create(400, 568, "ground").setScale(2).refreshBody(); //plataforma principal
+
+    //Mejora 4 (Mas plataformas)
+    this.platforms.create(600, 400, "ground");
+    this.platforms.create(50, 250, "ground");
+    this.platforms.create(750, 220, "ground");
   
 
     //Ninja player
