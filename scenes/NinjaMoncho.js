e152cc702e828f5cc542f15152f429b73fb2b05e
Evelyn-021 <evelynalonso11@hotmail.com>
2025-05-02 16:46:38 -0300

    Primera mejora

--- scenes/NinjaMoncho.js
+++ scenes/NinjaMoncho.js
@@ -8,10 +8,10 @@ export default class NinjaMoncho extends Phaser.Scene {
   }
 
   init() {
-    // this is called before the scene is created
     // init variables
-    // take data passed from other scenes
-    // data object param {}
+    this.tiempoRestante = 30; // segundos
+    this.textoTemporizador = null;
+    this.gameOver = false;
   }
 
   preload() {
@@ -71,56 +71,95 @@ export default class NinjaMoncho extends Phaser.Scene {
         // Grupo de ítems
             this.itemsGroup = this.physics.add.group();
 
-        // Hacer caer un ítem cada 0.5 segundos
-          this.time.addEvent({
-          delay: 500,
-          loop: true,
-          callback: () => {
-          const tipos = ["square", "triangle", "diamond"];
-          const tipo = Phaser.Utils.Array.GetRandom(tipos);
-          const x = Phaser.Math.Between(50, 750);
-            const item = this.itemsGroup.create(x, 0, tipo).setScale(1);
-          item.setData("tipo", tipo);
-          item.setVelocityY(150);
+            this.generadorItems = this.time.addEvent({
+              delay: 500,
+              loop: true,
+              callback: () => {
+                if (this.gameOver) return; // No generar más ítems si el juego ha terminado
+        
+                const tipos = ["square", "triangle", "diamond"];
+                const tipo = Phaser.Utils.Array.GetRandom(tipos);
+                const x = Phaser.Math.Between(50, 750);
+                const item = this.itemsGroup.create(x, 0, tipo).setScale(1);
+                item.setData("tipo", tipo);
+                item.setVelocityY(150);
+              }
+            });
+        
+            this.physics.add.overlap(this.Ninja, this.itemsGroup, (jugador, item) => {
+              const tipo = item.getData("tipo");
+              this.itemCounts[tipo]++;
+              item.destroy();
+        
+              this.scoreText.setText(
+                `Cuadrados: ${this.itemCounts.square}\nTriángulos: ${this.itemCounts.triangle}\nDiamantes: ${this.itemCounts.diamond}`
+              );
+        
+              if (
+                this.itemCounts.square >= 2 &&
+                this.itemCounts.triangle >= 2 &&
+                this.itemCounts.diamond >= 2
+              ) {
+                this.add.text(this.scale.width / 2, this.scale.height / 2, '¡GANASTE!', {
+                  fontSize: '64px',
+                  fill: '#0f0',
+                  fontStyle: 'bold',
+                  align: 'center'
+                }).setOrigin(0.5, 0.5);
+                this.scene.pause();
           }
         });
 
-        // Recolección
-          this.physics.add.overlap(this.Ninja, this.itemsGroup, (jugador, item) => {
-          const tipo = item.getData("tipo");
-          this.itemCounts[tipo]++;
-          item.destroy();
-
-          // Actualizar texto
-            this.scoreText.setText(
-            `Cuadrados: ${this.itemCounts.square}\nTriángulos: ${this.itemCounts.triangle}\nDiamantes: ${this.itemCounts.diamond}`
-          );
-
-          // Verificar victoria
-          if (
-            this.itemCounts.square >= 2 &&
-            this.itemCounts.triangle >= 2 &&
-            this.itemCounts.diamond >= 2
-          ) {
-            this.add.text(this.scale.width / 2, this.scale.height / 2, '¡GANASTE!', {
-              fontSize: '64px',
-              fill: '#0f0',
-              fontStyle: 'bold',
-              align: 'center'
-            }).setOrigin(0.5, 0.5); // Center the text
-            this.scene.pause();
-            
-            
-          }
-        });
-
+        
     
+
+
+
+        // TEMPORIZADOR AGREGADO //
+            this.textoTemporizador = this.add.text(this.scale.width - 16, 16, 'Tiempo: ' + this.tiempoRestante, {
+              fontSize: '32px',
+              fill: '#fff',
+            }).setOrigin(1, 0);
+
+            this.actualizarTemporizador = () => {
+              if (this.gameOver) return;
+
+              this.tiempoRestante--;
+              this.textoTemporizador.setText('Tiempo: ' + this.tiempoRestante);
+
+              if (this.tiempoRestante <= 0) {
+                this.tiempoRestante = 0;
+                this.physics.pause();
+                this.Ninja.setTint(0xff0000);
+                this.gameOver = true;
+
+                this.generadorItems.remove();
+
+                this.add.text(this.scale.width / 2, this.scale.height / 2, '¡PERDISTE!', {
+                  fontSize: '48px',
+                  fill: '#ff0000',
+                  fontStyle: 'bold'
+                }).setOrigin(0.5, 0.5);
+              }
+            };
+
+            this.time.addEvent({
+              delay: 1000,
+              callback: this.actualizarTemporizador,
+              callbackScope: this,
+              loop: true
+                        });
+
 
 
   }
 
   update() {// UPDATE GAME OBJECTS
 
+    if (this.gameOver) return;
+
+
+    
       //Movimiento hacia la izq.
       if (this.cursors.left.isDown) {
       this.Ninja.setVelocityX(-160);
